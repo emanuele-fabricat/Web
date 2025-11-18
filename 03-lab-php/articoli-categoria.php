f7e347aea68836a4ec35f6125b2a542af00af520
emanuele-fabricat <emanuele.fabricat@studio.unibo.it>
2025-11-18 14:47:52 +0100

    04 finish

--- 03-lab-php/articoli-categoria.php
+++ 03-lab-php/articoli-categoria.php
@@ -1,29 +1,25 @@
 <?php
-    require_once("bootstrap.php");
+require_once 'bootstrap.php';
 
-    //base template
-    $templateParams["titolo"] = "Blog Tw - Home";
-    $templateParams["nome"] = "lista-articoli.php";
-    $templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
-    $templateParams["categorie"] = $dbh->getCategories();
+//Base Template
+$templateParams["titolo"] = "Blog TW - Articoli Categoria";
+$templateParams["nome"] = "lista-articoli.php";
+$templateParams["categorie"] = $dbh->getCategories();
+$templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
+//Articoli Categoria Template
+$idcategoria = -1;
+if(isset($_GET["id"])){
+    $idcategoria = $_GET["id"];
+}
+$nomecategoria = $dbh->getCategoryById($idcategoria);
+if(count($nomecategoria)>0){
+    $templateParams["titolo_pagina"] = "Articoli della categoria ".$nomecategoria[0]["nomecategoria"];
+    $templateParams["articoli"] = $dbh->getPostByCategory($idcategoria);
+}
+else{
+    $templateParams["titolo_pagina"] = "Categoria non trovata"; 
+    $templateParams["articoli"] = array();   
+}
 
-    //tameplate specific
-    $templateParams["articoli"] = $dbh->getPosts(2);
-    $templateParams["titolo_pagina"] = "Ultimi Articoli";
-
-    $idCategoria = -1;
-    if (isset($_GET["id"])) {
-        $idCategoria = $_GET["id"];
-    }
-    $nomeCategoria = $dbh->getNameCategoryById($idCategoria);
-    if (count($nomeCategoria) > 0) {
-        $templateParams["titolo_pagina"] = "Articoli della categoria".$nomeCategoria[0]["nomecategoria"];
-        $templateParams["articoli"] = $dbh->getPostById($dbh->getCategoryByid($idCategoria));
-    } else {
-        $templateParams["titolo_pagina"] = "Categoria non trovata";
-        $templateParams["articoli"] = array();        
-    }
-
-
-    require("template/base.php")
+require 'template/base.php';
 ?>
--- 03-lab-php/db/database.php
+++ 03-lab-php/db/database.php
@@ -1,83 +1,84 @@
 <?php
-    class DatabaseHelper{
-        private $db;
+class DatabaseHelper{
+    private $db;
 
-        public function __construct($servername, $username, $password, $dbname, $port){
-            $this->db = new mysqli($servername, $username, $password, $dbname, $port);
-            if($this->db->connect_error){
-                die("Connessione al db fallita.");
-            }
-        }
-
-        public function getRandomPosts($n=2){
-            $stmt = $this->db->prepare("SELECT idarticolo, titoloarticolo, imgarticolo FROM articolo ORDER BY RAND() LIMIT ?");
-            $stmt->bind_param("i", $n);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getCategories(){
-            $stmt = $this->db->prepare("SELECT idcategoria, nomecategoria FROM categoria");
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getPosts($n=-1){
-            $query = "SELECT idarticolo, titoloarticolo, anteprimaarticolo, imgarticolo, dataarticolo, nome FROM articolo, autore WHERE autore=idautore ORDER BY dataarticolo DESC";
-            if($n>0){
-                $query .= " LIMIT ?";
-            }
-            $stmt = $this->db->prepare($query);
-            if($n>0){
-                $stmt->bind_param("i", $n);
-            }
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getAutors(){
-            $stmt = $this->db->prepare("SELECT nome, username, GROUP_CONCAT(DISTINCT nomeCategoria SEPARATOR ', ') AS nomeCategoria FROM autore, articolo, articolo_ha_categoria, categoria WHERE idautore=autore AND idarticolo=articolo AND idcategoria=categoria GROUP BY nome, username");
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getPostById($id){
-            $query = "SELECT idarticolo, titoloarticolo, testoarticolo, dataarticolo, imgarticolo, nome FROM articolo, autore WHERE autore = idautore AND idarticolo = ?";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('i', $id);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getCategoryByid($id) {
-            $query = "SELECT idarticolo FROM articolo, articolo_ha_categoria WHERE categoria = ? AND idarticolo = articolo";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('i', $id);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);
-        }
-
-        public function getNameCategoryById($id) {
-            $query = "SELECT nomecategoria FROM categoria WHERE idcategoria = ?";
-            $stmt = $this->db->prepare($query);
-            $stmt->bind_param('i', $id);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            return $result->fetch_all(MYSQLI_ASSOC);            
-        }
-
+    public function __construct($servername, $username, $password, $dbname){
+        $this->db = new mysqli($servername, $username, $password, $dbname);
+        if ($this->db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }        
     }
+
+    public function getRandomPosts($n){
+        $stmt = $this->db->prepare("SELECT idarticolo, titoloarticolo, imgarticolo FROM articolo ORDER BY RAND() LIMIT ?");
+        $stmt->bind_param('i',$n);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getCategories(){
+        $stmt = $this->db->prepare("SELECT * FROM categoria");
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getCategoryById($idcategory){
+        $stmt = $this->db->prepare("SELECT nomecategoria FROM categoria WHERE idcategoria=?");
+        $stmt->bind_param('i',$idcategory);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPosts($n=-1){
+        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore WHERE autore=idautore ORDER BY dataarticolo DESC";
+        if($n > 0){
+            $query .= " LIMIT ?";
+        }
+        $stmt = $this->db->prepare($query);
+        if($n > 0){
+            $stmt->bind_param('i',$n);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPostById($id){
+        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, testoarticolo, dataarticolo, nome FROM articolo, autore WHERE idarticolo=? AND autore=idautore";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getPostByCategory($idcategory){
+        $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore, articolo_ha_categoria WHERE categoria=? AND autore=idautore AND idarticolo=articolo";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i',$idcategory);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getAuthors(){
+        $query = "SELECT username, nome, GROUP_CONCAT(DISTINCT nomecategoria) as argomenti FROM categoria, articolo, autore, articolo_ha_categoria WHERE idarticolo=articolo AND categoria=idcategoria AND autore=idautore AND attivo=1 GROUP BY username, nome";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+
+}
 ?>
