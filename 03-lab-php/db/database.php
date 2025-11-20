7b2c5ff9f628ad98c32ede014379af295a95674f
emanuele-fabricat <emanuele.fabricat@studio.unibo.it>
2025-11-20 02:19:50 +0100

    ok

--- 03-lab-php/db/database.php
+++ 03-lab-php/db/database.php
@@ -1,24 +1,28 @@
 <?php
-class DatabaseHelper{
+class DatabaseHelper
+{
     private $db;
 
-    public function __construct($servername, $username, $password, $dbname){
+    public function __construct($servername, $username, $password, $dbname)
+    {
         $this->db = new mysqli($servername, $username, $password, $dbname);
         if ($this->db->connect_error) {
-            die("Connection failed: " . $db->connect_error);
-        }        
+            die("Connection failed: " . db->connect_error);
+        }
     }
 
-    public function getRandomPosts($n){
+    public function getRandomPosts($n)
+    {
         $stmt = $this->db->prepare("SELECT idarticolo, titoloarticolo, imgarticolo FROM articolo ORDER BY RAND() LIMIT ?");
-        $stmt->bind_param('i',$n);
+        $stmt->bind_param('i', $n);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getCategories(){
+    public function getCategories()
+    {
         $stmt = $this->db->prepare("SELECT * FROM categoria");
         $stmt->execute();
         $result = $stmt->get_result();
@@ -26,23 +30,25 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getCategoryById($idcategory){
+    public function getCategoryById($idcategory)
+    {
         $stmt = $this->db->prepare("SELECT nomecategoria FROM categoria WHERE idcategoria=?");
-        $stmt->bind_param('i',$idcategory);
+        $stmt->bind_param('i', $idcategory);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getPosts($n=-1){
+    public function getPosts($n = -1)
+    {
         $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore WHERE autore=idautore ORDER BY dataarticolo DESC";
-        if($n > 0){
+        if ($n > 0) {
             $query .= " LIMIT ?";
         }
         $stmt = $this->db->prepare($query);
-        if($n > 0){
-            $stmt->bind_param('i',$n);
+        if ($n > 0) {
+            $stmt->bind_param('i', $n);
         }
         $stmt->execute();
         $result = $stmt->get_result();
@@ -50,27 +56,30 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getPostById($id){
+    public function getPostById($id)
+    {
         $query = "SELECT idarticolo, titoloarticolo, imgarticolo, testoarticolo, dataarticolo, nome FROM articolo, autore WHERE idarticolo=? AND autore=idautore";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$id);
+        $stmt->bind_param('i', $id);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getPostByCategory($idcategory){
+    public function getPostByCategory($idcategory)
+    {
         $query = "SELECT idarticolo, titoloarticolo, imgarticolo, anteprimaarticolo, dataarticolo, nome FROM articolo, autore, articolo_ha_categoria WHERE categoria=? AND autore=idautore AND idarticolo=articolo";
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param('i',$idcategory);
+        $stmt->bind_param('i', $idcategory);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getAuthors(){
+    public function getAuthors()
+    {
         $query = "SELECT username, nome, GROUP_CONCAT(DISTINCT nomecategoria) as argomenti FROM categoria, articolo, autore, articolo_ha_categoria WHERE idarticolo=articolo AND categoria=idcategoria AND autore=idautore AND attivo=1 GROUP BY username, nome";
         $stmt = $this->db->prepare($query);
         $stmt->execute();
