55c3cde47ef106013f9dde5f95c7db26d3810a0f
emanuele-fabricat <emanuele.fabricat@studio.unibo.it>
2025-11-24 14:31:50 +0100

    start php pre exam

--- 06-lab-php/processa-articolo.php
+++ 06-lab-php/processa-articolo.php
@@ -7,7 +7,7 @@ if (!isUserLoggedIn() || !isset($_POST["action"])) {
     //header("location: login.php");
 }
 
-if ($_POST["action"]==1) {
+if ($_POST["action"] == 1) {
     //Inserimento
     $titoloarticolo = $_POST["titoloarticolo"];
     $testoarticolo = $_POST["testoarticolo"];
@@ -18,7 +18,7 @@ if ($_POST["action"]==1) {
     $categorie = $dbh->getCategories();
     $categorie_inserite = array();
     foreach ($categorie as $categoria) {
-        if (isset($_POST["categoria_".$categoria["idcategoria"]])) {
+        if (isset($_POST["categoria_" . $categoria["idcategoria"]])) {
             array_push($categorie_inserite, $categoria["idcategoria"]);
         }
     }
@@ -27,11 +27,11 @@ if ($_POST["action"]==1) {
     if ($result != 0) {
         $imgarticolo = $msg;
         $id = $dbh->insertArticle($titoloarticolo, $testoarticolo, $anteprimaarticolo, $dataarticolo, $imgarticolo, $autore);
-        if ($id!=false) {
-            foreach($categorie_inserite as $categoria) {
+        if ($id != false) {
+            foreach ($categorie_inserite as $categoria) {
                 $ris = $dbh->insertCategoryOfArticle($id, $categoria);
             }
-            $msg="Inserimento avvenuto correttamente";          
+            $msg = "Inserimento avvenuto correttamente";
         } else {
             $msg = "Errore in inserimento";
         }
